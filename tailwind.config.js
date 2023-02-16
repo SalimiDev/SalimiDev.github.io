02a38d73a32aaf0c4f9f6eada2faa0a33b0934d6
Mehdi Salimi <Salimi.devop@gmail.com>
2023-02-16 22:27:36 +0330

    build:added color palette and fonts to project

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,27 @@ module.exports = {
         './components/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: {
+                    50: '#FFFFFF',
+                    100: '#AFBBD4',
+                    200: '#8DA9F2',
+                    300: '#3C68D7',
+                    400: '#283044',
+                    500: '#12192B',
+                    600: '#0D152B',
+                    700: '#09122A',
+                    800: '#05112E',
+                    900: '#020E2C',
+                },
+            },
+        },
+        fontFamily: {
+            Montserrat: ['Montserrat', 'sans-serif'],
+            lato: ['Lato', 'sans-serif'],
+            nunito: ['Nunito', 'sans-serif'],
+        },
     },
     plugins: [],
 };
