3657433306940c36d84139f046f16d18aa888252
Mehdi Salimi <Salimi.devop@gmail.com>
2023-02-18 23:19:46 +0330

    feat:get pages title and added it to layout head to make it dynamic

--- components/layout.js
+++ components/layout.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import { useState, useEffect, useMemo, use, useCallback } from 'react';
+import { useRouter } from 'next/router';
 import Head from 'next/head';
 import AppSidebar from '@/components/AppSidebar';
 import Topbar from '@/components/Topbar';
 
 const Layout = ({ children }) => {
+    const [pageTitle, setPageTitle] = useState('Mehdi Salimi');
+    const { route } = useRouter();
+
+    const getPageTitle = useMemo(() => {
+        const pageTitles = {
+            '/': 'Home Page',
+            '/about': 'About Page',
+            '/skills': 'Skills Page',
+            '/projects': 'Projects Page',
+            '/contact': 'Contact Page',
+        };
+
+        return route => pageTitles[route] || 'Mehdi Salimi';
+    }, []);
+
+    const setPageTitleCallback = useCallback(() => {
+        setPageTitle(getPageTitle(route));
+    }, [getPageTitle, route]);
+
+    useEffect(() => {
+        setPageTitleCallback();
+    }, [setPageTitleCallback]);
+
     return (
         <div className='flex'>
             <Head>
-                <title>My App</title>
+                <title>{pageTitle}</title>
             </Head>
 
             <aside>
