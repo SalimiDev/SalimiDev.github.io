029a5d50ea62b3995fe5ed1baae3e6381e0e0d49
Mehdi Salimi <Salimi.devop@gmail.com>
2023-02-21 23:53:48 +0330

    fix:fixed style issue in home page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import SocialIcons from '@/components/SocialIcons';
 export default function Home() {
     return (
-        <div className='h-screen flex flex-col justify-center text-center p-4 space-y-8 md:space-y-10 font-nunito'>
+        <div className='h-[90%] flex flex-col justify-center text-center p-4 space-y-8 md:space-y-10 font-nunito'>
             <div className='space-y-4 md:space-y-6'>
                 <h2 className='text-primary-black dark:text-primary-white text-3xl sm:text-4xl md:text-5xl font-bold'>
                     Hi, I am <span className='text-primary-300'>Mehdi Salimi</span>
