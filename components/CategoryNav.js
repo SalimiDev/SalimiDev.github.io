1b243b667d69a74e4a84b970f18c2426fe1d76be
Mehdi Salimi <Salimi.devop@gmail.com>
2023-03-07 00:07:23 +0330

    style:update some styles

--- components/CategoryNav.js
+++ components/CategoryNav.js
@@ -7,7 +7,7 @@ const CategoryNav = ({ tabList, activeTab, handleFilter }) => {
                 {tabList.map((tabName, index) => (
                     <li
                         key={index}
-                        className={`px-3 py-2 mb-3 text-primary-white  font-lato select-none shadow-slate-900 shadow-md hover:scale-105 hover:bg-yellow-300  hover:text-primary-500 transition duration-200 cursor-pointer ${
+                        className={`px-3 py-2 rounded-sm mb-3 text-primary-white  font-lato select-none shadow-slate-900 shadow-md hover:scale-105 hover:bg-yellow-300  hover:text-primary-500 transition duration-200 cursor-pointer ${
                             activeTab === tabName ? 'bg-yellow-300 text-primary-500' : 'bg-primary-300'
                         }`}
                         onClick={() => handleFilter(tabName)}>
--- pages/about.js
+++ pages/about.js
@@ -43,7 +43,7 @@ const About = () => {
 
                         <button
                             style={{ marginTop: '40px' }}
-                            className='px-4 py-2 lg:px-6 lg:py-3 bg-primary-300 text-primary-white flex gap-2'>
+                            className='px-4 py-2 lg:px-6 lg:py-3 bg-primary-300 text-primary-white flex gap-2 rounded-sm'>
                             DOWNLOAD RESUME
                             <span>
                                 <FaDownload />
