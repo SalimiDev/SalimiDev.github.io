b3250feb6b7862b22cd3b537392200d783b62f55
Mehdi Salimi <Salimi.devop@gmail.com>
2023-03-16 04:32:35 +0330

    feat:created contact form

--- pages/contact.js
+++ pages/contact.js
@@ -3,7 +3,22 @@ import { TfiLocationPin, TfiEmail } from 'react-icons/tfi';
 import { IoCallOutline } from 'react-icons/io5';
 import { FaWhatsapp } from 'react-icons/fa';
 
+import { useForm } from 'react-hook-form';
+
 const Contact = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+    const onSubmit = () => {
+        console.log('first');
+    };
+
+    const errorStyle = 'text-red-600 text-sm';
+    const inputStyle = 'w-full p-1 border-none bg-primary-100 dark:bg-primary-200';
+
     return (
         <div className='w-full px-4 space-y-6 mb-16'>
             <Header
@@ -11,7 +26,7 @@ const Contact = () => {
                 pageDesc={"Let's Get in Touch - Reach out to me and let's discuss how we can collaborate or work together"}
             />
 
-            <div className=' w-full space-y-5 py-12 dark:text-primary-100'>
+            <section className='w-full space-y-5 py-12 dark:text-primary-100'>
                 <span className='flex gap-3'>
                     <IoCallOutline size={25} color='white' /> <a href='tel:+989370938781'>+98-937-093-8781</a>
                 </span>
@@ -24,7 +39,31 @@ const Contact = () => {
                 <span className='flex gap-3'>
                     <TfiLocationPin size={25} color='white' /> <p>Alborz , Iran</p>
                 </span>
-            </div>
+            </section>
+
+            <section className='space-y-3'>
+                <h3 className='mb-8'>Get in Touch</h3>
+                <h4 className='flex justify-start'>Leave me a Message</h4>
+
+                <form onSubmit={handleSubmit(onSubmit)} className='w-full space-y-2'>
+                    <input {...register('fullName', { required: true })} placeholder='Full Name' className={inputStyle} />
+                    {errors.fullName && <p className={errorStyle}>This field is required!</p>}
+
+                    <input {...register('email', { required: true })} placeholder='Email' className={inputStyle} />
+                    {errors.email && <p className={errorStyle}>Please enter your email!</p>}
+
+                    <textarea
+                        {...register('message', { required: true })}
+                        cols='30'
+                        rows='10'
+                        placeholder='Message'
+                        className={inputStyle}
+                    />
+                    {errors.message && <p className={errorStyle}>Please enter your message.</p>}
+
+                    <input type='submit' className='w-full py-2 bg-primary-300 ' />
+                </form>
+            </section>
         </div>
     );
 };
