cf1a489ae0330734b0a7cdcf1c45d475a0d2eb7e
Mehdi Salimi <Salimi.devop@gmail.com>
2023-03-18 04:37:11 +0330

    style:fixed the responive design of the contact page

--- pages/contact.js
+++ pages/contact.js
@@ -43,80 +43,87 @@ const Contact = () => {
 
     const errorStyle = 'text-red-600 text-sm';
     const inputStyle =
-        'w-full p-1 border-none bg-primary-100 dark:bg-primary-200 focus:outline-none focus:shadow-inner focus:shadow-gray-300';
+        'w-full p-1 border-none bg-primary-100 dark:bg-primary-200 focus:outline-none focus:shadow-inner focus:shadow-gray-300 xl:py-2';
 
     return (
-        <div className='w-full px-4 space-y-6 mb-16'>
+        <div className='w-full px-4 space-y-16 lg:px-8 xl:px-10'>
             <Header
                 pageTitle={'Contact Me'}
                 pageDesc={"Let's Get in Touch - Reach out to me and let's discuss how we can collaborate or work together"}
             />
 
-            <section className='w-full space-y-5 py-12 dark:text-primary-100'>
-                <span className='flex gap-3'>
-                    <IoCallOutline size={25} color='white' /> <a href='tel:+989370938781'>+98-937-093-8781</a>
-                </span>
-                <span className='flex gap-3'>
-                    <TfiEmail size={23} color='white' /> <a href='mailto:salimi.devop@gmail.com'>salimi.devop@gmail.com</a>
-                </span>
-                <span className='flex gap-3'>
-                    <FaWhatsapp size={23} color='white' /> <a href='https://wa.me/+989370938781'>Send Message on WhatsApp</a>
-                </span>
-                <span className='flex gap-3'>
-                    <TfiLocationPin size={25} color='white' /> <p>Alborz , Iran</p>
-                </span>
-            </section>
+            <div className='space-y-16 flex felx-col flex-wrap sm:flex-row xl:flex-row xl:space-y-8'>
+                <div className='w-full space-y-5 flex flex-col flex-wrap justify-center align-middle sm:flex-row xl:w-1/2 xl:flex-col xl:space-y-0'>
+                    <section className='w-full space-y-5  dark:text-primary-100 sm:w-[50%] sm:py-16 xl:py-10'>
+                        <h6 className='flex gap-5'>
+                            <IoCallOutline size={25} className='dark:text-white' />
+                            <a href='tel:+989370938781'>+98-937-093-8781</a>
+                        </h6>
+                        <h6 className='flex gap-5'>
+                            <TfiEmail size={23} className='dark:text-white' />
+                            <a href='mailto:salimi.devop@gmail.com'>salimi.devop@gmail.com</a>
+                        </h6>
+                        <h6 className='flex gap-5'>
+                            <FaWhatsapp size={25} className='dark:text-white' />
+                            <a href='https://wa.me/+989370938781'>Send Message on WhatsApp</a>
+                        </h6>
+                        <h6 className='flex gap-5'>
+                            <TfiLocationPin size={26} className='dark:text-white' /> <p>Alborz , Iran</p>
+                        </h6>
+                    </section>
 
-            <section>
-                <Map />
-            </section>
+                    <section className='sm:w-1/2 xl:w-3/4 xl:h-[250px]'>
+                        <Map />
+                    </section>
+                </div>
 
-            <section className='space-y-3'>
-                <h3 className='mb-8'>Get in Touch</h3>
-                <h4 className='flex justify-start'>Leave me a Message</h4>
+                <section className='w-full space-y-3 xl:w-1/2 xl:space-y-6'>
+                    <h3 className='mb-8 xl:hidden'>Get in Touch</h3>
+                    <h4 className='flex justify-start'>Leave me a Message</h4>
 
-                <form onSubmit={handleSubmit(onSubmit)} autocomplete='off' className='w-full space-y-2'>
-                    <input {...register('fullName')} autocomplete='new' placeholder='Full Name' className={inputStyle} />
-                    <input {...register('email')} placeholder='Email' className={inputStyle} />
-                    <textarea {...register('message')} rows='10' placeholder='Message' className={inputStyle} />
-                    <input type='submit' className='w-full py-2 bg-primary-300 ' />
+                    <form onSubmit={handleSubmit(onSubmit)} className='w-full space-y-2'>
+                        <input {...register('fullName')} autocomplete='new' placeholder='Full Name' className={inputStyle} />
+                        <input {...register('email')} autocomplete='new' placeholder='Email' className={inputStyle} />
+                        <textarea {...register('message')} rows='10' placeholder='Message' className={inputStyle} />
+                        <input type='submit' className='w-full py-2 bg-primary-300 ' />
 
-                    <AnimatePresence>
-                        <ul className='w-full px-1 space-y-1 bg-red-200 list-disc overflow-hidden'>
-                            {errors?.fullName?.message && (
-                                <motion.li
-                                    variants={errorEffect}
-                                    initial='initial'
-                                    animate='animate'
-                                    className='flex gap-1 py-1'>
-                                    <label className='text-red-700 text-sm font-bold'>● Full Name: </label>
-                                    {<p className={errorStyle}>{errors?.fullName?.message}</p>}
-                                </motion.li>
-                            )}
-                            {errors?.email?.message && (
-                                <motion.li
-                                    variants={errorEffect}
-                                    initial='initial'
-                                    animate='animate'
-                                    className='flex gap-1 py-1'>
-                                    <label className='text-red-700 text-sm font-bold'>● Email:</label>
-                                    {<p className={errorStyle}>{errors?.email?.message}</p>}
-                                </motion.li>
-                            )}
-                            {errors?.message?.message && (
-                                <motion.li
-                                    variants={errorEffect}
-                                    initial='initial'
-                                    animate='animate'
-                                    className='flex gap-1 py-1'>
-                                    <label className='text-red-700 text-sm font-bold'>● Message: </label>
-                                    {<p className={errorStyle}>{errors?.message?.message}</p>}
-                                </motion.li>
-                            )}
-                        </ul>
-                    </AnimatePresence>
-                </form>
-            </section>
+                        <AnimatePresence>
+                            <ul className='w-full px-1 space-y-1 bg-red-200 list-disc overflow-hidden'>
+                                {errors?.fullName?.message && (
+                                    <motion.li
+                                        variants={errorEffect}
+                                        initial='initial'
+                                        animate='animate'
+                                        className='flex gap-1 py-1'>
+                                        <label className='text-red-700 text-sm font-bold'>● Full Name: </label>
+                                        {<p className={errorStyle}>{errors?.fullName?.message}</p>}
+                                    </motion.li>
+                                )}
+                                {errors?.email?.message && (
+                                    <motion.li
+                                        variants={errorEffect}
+                                        initial='initial'
+                                        animate='animate'
+                                        className='flex gap-1 py-1'>
+                                        <label className='text-red-700 text-sm font-bold'>● Email:</label>
+                                        {<p className={errorStyle}>{errors?.email?.message}</p>}
+                                    </motion.li>
+                                )}
+                                {errors?.message?.message && (
+                                    <motion.li
+                                        variants={errorEffect}
+                                        initial='initial'
+                                        animate='animate'
+                                        className='flex gap-1 py-1'>
+                                        <label className='text-red-700 text-sm font-bold'>● Message: </label>
+                                        {<p className={errorStyle}>{errors?.message?.message}</p>}
+                                    </motion.li>
+                                )}
+                            </ul>
+                        </AnimatePresence>
+                    </form>
+                </section>
+            </div>
         </div>
     );
 };
