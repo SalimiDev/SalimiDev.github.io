7e55522697f7fe194ccaaee71ea6c5253b8ee511
Mehdi Salimi <Salimi.devop@gmail.com>
2023-04-12 13:20:09 +0330

    feat:created Slider with swiperjs

---
+++ components/Slider.js
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/bundle';
+import 'swiper/css/zoom';
+import { Navigation, Pagination, Scrollbar, A11y, Thumbs, Autoplay } from 'swiper';
+import Image from 'next/image';
+
+const Slider = ({ images }) => {
+    // store thumbs swiper instance
+    const [thumbsSwiper, setThumbsSwiper] = useState(null);
+
+    const mainSwiperOptions = {
+        spaceBetween: 30,
+        slidesPerView: 1,
+        loop: true,
+        grabCursor: true,
+        navigation: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        pagination: { clickable: true },
+        scrollbar: { draggable: true },
+    };
+
+    const thumbsSwiperParams = {
+        slidesPerView: 5,
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+        spaceBetween: 10,
+        breakpoints: {
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 6,
+            },
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 10,
+            },
+            1024: {
+                slidesPerView: 5,
+                spaceBetween: 15,
+            },
+        },
+    };
+
+    return (
+        <div className='w-1/2 px-2'>
+            <Swiper modules={[Navigation, Pagination, Scrollbar, A11y, Thumbs, Autoplay]} {...mainSwiperOptions}>
+                {images.map(image => (
+                    <SwiperSlide key={image.id}>
+                        <img src={image.src} alt={image.alt} />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+
+            <Swiper modules={[Thumbs]} onSwiper={setThumbsSwiper} {...thumbsSwiperParams}>
+                {images.map(image => (
+                    <SwiperSlide key={image.id}>
+                        <img src={image.src} alt={image.alt} />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </div>
+    );
+};
+
+export default Slider;
--- pages/projects/[projectDetail].js
+++ pages/projects/[projectDetail].js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import axios from 'axios';
 import { VscLoading } from 'react-icons/vsc';
+import Slider from '@/components/Slider';
 
 const ProjectDetail = () => {
     const [project, setProject] = useState(null);
@@ -32,9 +33,31 @@ const ProjectDetail = () => {
         );
     }
 
+    const images = [
+        {
+            id: 1,
+            src: 'https://cdn.pixabay.com/photo/2023/02/04/13/47/butterfly-7767228_1280.jpg',
+            thumb: 'https://cdn.pixabay.com/photo/2023/02/04/13/47/butterfly-7767228_1280.jpg',
+            alt: 'Image 1',
+        },
+        {
+            id: 2,
+            src: 'https://cdn.pixabay.com/photo/2023/03/22/20/16/muffin-7870491_1280.jpg',
+            thumb: '/https://cdn.pixabay.com/photo/2023/03/22/20/16/muffin-7870491_1280.jpg',
+            alt: 'Image 2',
+        },
+        {
+            id: 3,
+            src: 'https://cdn.pixabay.com/photo/2023/03/27/13/48/squirrel-7880791_1280.jpg',
+            thumb: '/https://cdn.pixabay.com/photo/2023/03/27/13/48/squirrel-7880791_1280.jpg',
+            alt: 'Image 3',
+        },
+    ];
+
     return (
         <div>
             <h2>{project.title}</h2>
+            <Slider  images={images} />
         </div>
     );
 };
