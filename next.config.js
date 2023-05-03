180be42c0d3e93a3fa7751151681e6fdda57d51c
Mehdi Salimi <Salimi.devop@gmail.com>
2023-05-03 04:27:07 +0330

    test slider component that might cause error in build time

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    output: 'export',
     swcMinify: true,
     optimizeFonts: true,
     images: {
-        loader: 'imgix',
-        path: '/assets',
-        unoptimized: true,
         remotePatterns: [
             {
                 protocol: 'https',
